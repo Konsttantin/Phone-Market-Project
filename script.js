2f693cfa81021068e395a82ec5de0d300615e881
Konstantin <pozh.bioptat@gmail.com>
2023-03-11 19:22:24 +0200

    switching filters was added

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 'use strict'
 
 const filters = document.querySelector('.filters');
+const sortOptions = document.querySelector('.sort-options');
+let currentSortOption = document.getElementById('default');
 
 filters.addEventListener('click', (e) => {
   const description = e.target.closest('.filter__description');
 
   if (description) {
     description.classList.toggle('active');
+  }
+});
+
+sortOptions.addEventListener('click', (e) => {
+  const sortOption = e.target.closest('.sort-options__item');
+
+  if (sortOption) {
+    currentSortOption.classList.remove('active');
+    sortOption.classList.toggle('active');
+
+    currentSortOption = sortOption;
   }
 });
