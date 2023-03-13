ab74850413a184ef01d77ec788593fdd052dffe4
Konstantin <pozh.bioptat@gmail.com>
2023-03-13 23:40:45 +0200

    added 15 products

--- Js/goods.js
+++ Js/goods.js
@@ -1,7 +1,7 @@
 export const goods = [
   {
     id: 1,
-    name: 'Смартфон Apple iPhone 11 128GB Black',
+    name: 'Apple iPhone 11 128GB Black',
     img: 'https://img.moyo.ua/img/products/4524/71_200.jpeg?1678475815',
     rating: 0,
     price: 25499,
@@ -16,7 +16,7 @@ export const goods = [
   
   {
     id: 2,
-    name: 'Смартфон Apple iPhone 14 Pro Max 1TB Space Black (MQC23RX/A)',
+    name: 'Apple iPhone 14 Pro Max 1TB Space Black',
     img: 'https://img.moyo.ua/img/products/5237/21_200.jpg?1678628929',
     rating: 0,
     price: 79999,
@@ -31,16 +31,196 @@ export const goods = [
   
   {
     id: 3,
-    name: '',
-    img: '',
+    name: 'Apple iPhone 12 64GB Black',
+    img: 'https://img.moyo.ua/img/products/4772/21_200.jpg?1678715237',
     rating: 0,
-    price: '',
+    price: 27499,
     favourite: false,
-    display: '',
-    memory: '',
-    ram: '',
-    sim: '',
+    display: 6.1,
+    memory: 64,
+    ram: 4,
+    sim: 1,
     prod: 'APPLE',
-    security: ''
+    security: 'face'
+  },
+
+  {
+    id: 4,
+    name: 'Apple iPhone 13 128Gb Blue',
+    img: 'https://img.moyo.ua/img/products/4988/83_200.jpg?1678715254',
+    rating: 0,
+    price: 31999,
+    favourite: false,
+    display: 6.1,
+    memory: 128,
+    ram: 6,
+    sim: 1,
+    prod: 'APPLE',
+    security: 'face'
+  },
+
+  {
+    id: 5,
+    name: 'Apple iPhone 13 mini 256Gb (PRODUCT) RED',
+    img: 'https://img.moyo.ua/img/products/4989/5_200.jpg?1678715238',
+    rating: 0,
+    price: 33999,
+    favourite: false,
+    display: 5.4,
+    memory: 256,
+    ram: 4,
+    sim: 1,
+    prod: 'APPLE',
+    security: 'face'
+  },
+
+  {
+    id: 6,
+    name: 'Apple iPhone 14 512GB Midnight',
+    img: 'https://img.moyo.ua/img/products/5236/70_200.jpg?1678715238',
+    rating: 0,
+    price: 50999,
+    favourite: false,
+    display: 6.1,
+    memory: 512,
+    ram: 8,
+    sim: 2,
+    prod: 'APPLE',
+    security: 'face'
+  },
+
+  {
+    id: 7,
+    name: 'Apple iPhone SE 2022 64GB Midnight',
+    img: 'https://img.moyo.ua/img/products/5287/22_200.jpg?1678715237',
+    rating: 0,
+    price: 21999,
+    favourite: false,
+    display: 4.7,
+    memory: 64,
+    ram: 3,
+    sim: 1,
+    prod: 'APPLE',
+    security: 'touch'
+  },
+
+  {
+    id: 8,
+    name: 'Apple iPhone 12 128GB White',
+    img: 'https://img.moyo.ua/img/products/4772/27_200.jpg?1678715237',
+    rating: 0,
+    price: 29999,
+    favourite: false,
+    display: 6.1,
+    memory: 128,
+    ram: 4,
+    sim: 1,
+    prod: 'APPLE',
+    security: 'face'
+  },
+
+  {
+    id: 9,
+    name: 'Apple iPhone 11 64GB Yellow',
+    img: 'https://img.moyo.ua/img/products/4524/69_200.jpg?1678715297',
+    rating: 0,
+    price: 22499,
+    favourite: false,
+    display: 6.1,
+    memory: 64,
+    ram: 4,
+    sim: 1,
+    prod: 'APPLE',
+    security: 'face'
+  },
+
+  {
+    id: 10,
+    name: 'Apple iPhone SE 2022 128GB Starlight',
+    img: 'https://img.moyo.ua/img/products/5288/87_200.jpg?1678715450',
+    rating: 0,
+    price: 24499,
+    favourite: false,
+    display: 4.7,
+    memory: 128,
+    ram: 4,
+    sim: 1,
+    prod: 'APPLE',
+    security: 'touch'
+  },
+
+  {
+    id: 11,
+    name: 'Samsung Galaxy S23 Ultra 12/512Gb Green',
+    img: 'https://img.moyo.ua/img/products/5344/96_200.jpg?1678715245',
+    rating: 0,
+    price: 61499,
+    favourite: false,
+    display: 6.8,
+    memory: 512,
+    ram: 12,
+    sim: 2,
+    prod: 'SAMSUNG',
+    security: 'face'
+  },
+
+  {
+    id: 12,
+    name: 'Samsung Galaxy Fold4 F936B/256 Phantom Black',
+    img: 'https://img.moyo.ua/img/products/5215/85_200.jpg?1678715239',
+    rating: 0,
+    price: 60499,
+    favourite: false,
+    display: 7.6,
+    memory: 256,
+    ram: 12,
+    sim: 2,
+    prod: 'SAMSUNG',
+    security: 'face'
+  },
+
+  {
+    id: 13,
+    name: 'Samsung Galaxy S23+ 8/256Gb Green',
+    img: 'https://img.moyo.ua/img/products/5345/4_200.jpg?1678715240',
+    rating: 0,
+    price: 47999,
+    favourite: false,
+    display: 6.6,
+    memory: 256,
+    ram: 8,
+    sim: 2,
+    prod: 'SAMSUNG',
+    security: 'face'
+  },
+
+  {
+    id: 14,
+    name: 'Samsung Galaxy S23 8/256Gb Light Pink',
+    img: 'https://img.moyo.ua/img/products/5345/6_200.jpg?1678715240',
+    rating: 0,
+    price: 41999,
+    favourite: false,
+    display: 6.1,
+    memory: 256,
+    ram: 8,
+    sim: 2,
+    prod: 'SAMSUNG',
+    security: 'face'
+  },
+
+  {
+    id: 15,
+    name: 'Samsung Galaxy Flip4 F721B/256 Bora Purple',
+    img: 'https://img.moyo.ua/img/products/5215/87_200.jpg?1678715239',
+    rating: 0,
+    price: 38699,
+    favourite: false,
+    display: 6.7,
+    memory: 256,
+    ram: 8,
+    sim: 2,
+    prod: 'SAMSUNG',
+    security: 'face'
   },
 ];
