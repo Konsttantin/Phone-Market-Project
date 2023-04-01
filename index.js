030df7e05ca4a4b92c20be7d55db7b5440b1e5e7
Konstantin <pozh.bioptat@gmail.com>
2023-04-01 17:08:06 +0300

    added express + nodemon

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const app = express();
+const PORT = process.env.PORT || 80;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log('OPEN --------->', 'http://localhost/');
+});
